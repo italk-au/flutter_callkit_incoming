49171414f769bd93086d58c39af67a93e1ed098d
bitgooph <bitgooph@gmail.com>
2024-01-15 04:47:34 +0800

    Added back log import

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingBroadcastReceiver.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingBroadcastReceiver.kt
@@ -14,6 +14,7 @@ import org.json.JSONObject
 import java.io.IOException
 import java.net.URL
 import java.io.File
+import android.util.Log
 
 class CallkitIncomingBroadcastReceiver : BroadcastReceiver() {
 
